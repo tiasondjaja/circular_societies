aea378638bc44b5d489ceed61a09cbec599cf648
Tia Sondjaja <msondjaja@gmail.com>
2025-03-18 13:54:17 -0400

    added one more plot, combining two count functions

--- functional_approval.py
+++ functional_approval.py
@@ -7,16 +7,17 @@ import itertools
 # Define the ApprovalSet class (approval set)
 class ApprovalSet:
     # Initialize Class
-    def __init__( self, name, function ):
+    def __init__( self, name, function, xmin=0, xmax=10 ):
         self.name = name
         self.function = function
-  
+        self.left_endpt, self.right_endpt = self.find_support(xmin=xmin, xmax=xmax)
+
     def edit_set( self, newname, newfunction ):
         self.name = newname
         self.function = newfunction
-    
-    def find_support( self, xmin, xmax ):
-        x = np.linspace(xmin, xmax, 100)
+
+    def find_support( self, xmin=0, xmax=10, prec=0.001 ):
+        x = np.arange(xmin, xmax+prec, prec)
         index_nonzero = [ i for i, pt in enumerate(x) if self.function(pt) > 0 ]
         left_endpt = x[min(index_nonzero)]
         right_endpt = x[max(index_nonzero)]
@@ -24,133 +25,230 @@ class ApprovalSet:
 
     def is_point_in_support( self, point):
         check = self.function(point) > 0
-        
+
         return check
-  
-# Part 1 - MAIN ----------------------------------  
+
+# Part 1 - MAIN ----------------------------------
 
 # Define the Society class
 
 class Society:
-    
+
     # Initialize Class
-    def __init__( self, name, tick = 0.5 ):
+    def __init__( self, name=None, prec=0.001 ):
         self.name = name
-        self.approval_sets = [] # initialize with empty approval sets
+        self.approval_sets = dict() # initialize with an empty collection of approval sets
         self.num_voters = 0
-        self.tick = tick
-        
-        # List of all endpoints and setnames (useful when checking for left-right alternation)
-        self.list_setnames = []
-   
-    ## DISPLAYING / OBTAINING BASIC INFORMATION ABOUT THE CIRCULAR SOCIETY----         
+
+        # List of endpoint information
+        self.xmin = None
+        self.xmax = None
+        self.ymin = None
+        self.ymax = None
+
+        # Precision used for plotting
+        self.prec = prec
+
+        # Count functions
+        self.X = None
+        self.count_function = None
+        self.binary_count_function = None
+
+    ## DISPLAYING / OBTAINING BASIC INFORMATION ABOUT THE CIRCULAR SOCIETY----
     # Method to print society information
     def print_society_info( self ):
-        print("Circular Society Name: " + self.name)
-        print("Number of Voters: " + str(self.num_voters) )
-        print(f"Approval Set names: {self.list_setnames}" )
-   
-    ### FINDING, EDITING, ADDING, REMOVING APPROVAL SETS-----------------------
-    def find_approval_set( set_name ):
-        for ind, A in enumerate( self.approval_sets ):
-            if set_name == A.name:
-                return ind
-        return -1
+        print(f"Society Name: {self.name}")
+        print(f"Number of Voters: {self.num_voters}" )
+        print(f"Approval Set names: {self.approval_sets.keys()}" )
+        print(f"xmin: {self.xmin}, xmax={self.xmax}, ymin={self.ymin}, ymax={self.ymax}")
 
-    # Method to add a new approval set into the society
+    ### FINDING, EDITING, ADDING, REMOVING APPROVAL SETS-----------------------
+    def get_approval_set( self, set_name ):
+        if set_name in self.approval_sets.keys():
+            return self.approval_sets[set_name]
+        else:
+            return None
+
+    def update_spectrum( self ):
+        if len(self.approval_sets) == 0:
+            self.xmin = None
+            self.xmax = None
+            self.ymin = None
+            self.ymax = None
+        else:
+            self.xmin = np.min( np.array([A.left_endpt for A in self.approval_sets.values()]) )
+            self.xmax = np.max( np.array([A.right_endpt for A in self.approval_sets.values()]) )
+            Y = np.array([ 
+                A.function(pt) for A in self.approval_sets.values()
+                for pt in np.arange(A.left_endpt, A.right_endpt+self.prec, self.prec)
+           ])
+            self.ymin = min(0, np.min(Y))
+            self.ymax = np.max(Y)
+
+    def update_count_functions( self ):
+        if len(self.approval_sets) == 0:
+            self.X = None
+            self.count_function = None
+        else:
+            self.X = np.arange(self.xmin, self.xmax + self.prec, self.prec)
+            Y1 = np.zeros_like(self.X)
+            Y2 = np.zeros_like(self.X)
+            for A in self.approval_sets.values():
+                Y1 = Y1 + np.array([ A.function(pt) for pt in self.X])
+                Y2 = Y2 + np.array([ 1 if A.function(pt)>0 else 0 for pt in self.X])
+            self.count_function = Y1
+            self.binary_count_function = Y2
+
     def add_approval_set( self, set_name, approval_function ):
         new_set = ApprovalSet( set_name, approval_function)
         # Update attributes
-        self.approval_sets.append( new_set )
+        self.approval_sets[set_name] = new_set
         self.num_voters += 1
-        
-        # Update list of all endpoints
-        self.list_setnames.append( set_name )
-               
-    # Method to remove an approval set from the society
-    def removeApprovalSet( self, set_name ):
-        for ind, A in enumerate( self.approval_sets ):
-            if set_name == A.name:
-                self.approval_sets.pop(ind)
-                self.list_setnames.pop(ind)
-                self.num_voters -= 1
-    
-    # Method to edit the endpoints of an approval set
-    def editApprovalSet( self, set_name, approval_function ):
-        ind = self.find_approval_set( set_name ) # if not found, ind = -1
-        
-        if ind == -1:
+        self.update_spectrum()
+        self.update_count_functions()
+
+    def remove_approval_set( self, set_name ):
+        self.approval_sets.pop(set_name)
+        self.num_voters -= 1
+        self.update_spectrum()
+        self.update_count_functions()
+
+    def edit_approval_set( self, set_name, approval_function ):
+        A = self.get_approval_set( set_name ) # if not found, A is None
+        if A is None:
             print("Set name is not found")
         else:
-            A = self.approvalSets[ind]
-            A.editSet( set_name, approval_function )
-       
+            A.edit_set( set_name, approval_function )
+        self.update_spectrum()
+        self.update_count_function()
+
+
     ### VISUALIZATION --------------------------------------------------------
-    
-    # Method to visualize the sets
-    def visualize( self, fig_path=None, xmin=0, xmax=10, ymin=0, ymax=5 ):
-        
-        ## Set up grid
-        fig = plt.figure()
-        ax = fig.add_subplot(2, 1, 1)
-        # Major ticks every 1, minor ticks every self.tick (default = 0.5)
-        major_ticks = np.arange(xmin, xmax, self.tick * 2)
-        minor_ticks = np.arange(xmin, xmax, self.tick)        
+    def setup_grid( self, ax, xmin, xmax, ymin, ymax, prec, tick ):
+        major_ticks = np.arange(xmin, xmax+prec, 1)
+        minor_ticks = np.arange(xmin, xmax+prec, tick)
+       # Set up grid
         ax.set_xticks(major_ticks)
         ax.set_xticks(minor_ticks, minor=True)
-        #ax.set_yticks(major_ticks)
-        #ax.set_yticks(minor_ticks, minor=True)
         # Different thickness settings for the major and minor grids:
         ax.grid(which='minor', alpha=0.2)
         ax.grid(which='major', alpha=0.5)
-        plt.yticks(np.arange(0, self.num_voters+1, step = 1) )
+        ax.set_ylim(ymin, ymax+0.5)
+        ax.set_yticks(np.arange(ymin, ymax+prec, 1))
+        ax.set_yticks(np.arange(ymin, ymax, 0.5), minor=True)
 
-        # colormap
-        colors = plt.cm.jet(np.linspace(0, 1, self.num_voters) )
-        
-        # Draw the functions
-        for ind, A in enumerate( self.approval_sets ):
-            left_endpt, right_endpt = A.find_support(xmin, xmax)
-            x = np.linspace(left_endpt, right_endpt, 10)
-            y = [A.function(pt) for pt in x]
-            plt.plot( x, y, color=colors[ind], label=A.name)
-            plt.plot( left_endpt, A.function(left_endpt), color=colors[ind], marker="o" )
-            plt.plot( right_endpt, A.function(right_endpt), color=colors[ind], marker="o" )
+    def plot_summary_function( self, ax, X, Y, label, tick=1/3):
+        max_Y = np.max(Y)
+        argmax_Y = [ x for x, y in zip(X, Y) if y==max_Y]
+        ymax = max(self.ymax, max_Y+1)
 
-        ax.set_ylim(ymin, ymax)
-        ax.legend(bbox_to_anchor = [1, 1], loc = 'center left' )
+        ax.plot( X, Y , label=label )
+        ax.set_title( label )
+        ax.axvline(
+            x=argmax_Y[0], ymin=self.ymin, ymax=ymax,
+            linestyle='--', color='red', linewidth=0.5
+        )
+        if len(argmax_Y) > 1:
+            ax.axvline(
+                x=argmax_Y[-1], ymin=self.ymin, ymax=ymax,
+                linestyle='--', color='red', linewidth=0.5
+            )
+        ax.axhline(
+            y=max_Y, xmin=self.xmin, xmax=self.xmax,
+            linestyle='--', color='red', linewidth=0.5
+        )
+        ax.text(x = 0, y = max_Y, s=str(round(max_Y, 2)), color='red')
+        ax.legend(bbox_to_anchor = [1, 1], loc = 'upper left' )
+        self.setup_grid(ax, self.xmin, self.xmax, self.ymin, ymax, self.prec, tick )
+
+
+    # Method to visualize the sets, count function, and approval count
+    def visualize(
+            self,
+            plot_approval_sets = True ,
+            plot_count_function = True,
+            plot_binary_count = True,
+            plot_sum_count = True,
+            fig_path=None,
+            xmin=None, xmax=None, ymin=None, ymax=None,
+            tick=1/3, prec=None      
+        ):
+
+        if xmin is None:
+            xmin = self.xmin
+        if xmax is None:
+            xmax = self.xmax
+        if ymin is None:
+            ymin = self.ymin
+        if ymax is None:
+            ymax = self.ymax
+        if prec is not None:
+            self.prec = prec
+
+        ## Plot Setup
+        nrows = int(plot_approval_sets)+int(plot_count_function) + int(plot_binary_count) + int(plot_sum_count)
+        fig, axs = plt.subplots(nrows = nrows, ncols = 1, sharex=True)
+        plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=0.75)
+
+        r = 0
+        if plot_approval_sets:
+            # Draw the functions
+            colors = plt.cm.jet(np.linspace(0, 1, self.num_voters) )
+            for ind, A in enumerate(self.approval_sets.values()):
+                x = np.arange(A.left_endpt, A.right_endpt+self.prec, self.prec )
+                y = [A.function(pt) for pt in x]
+                axs[r].plot( x, y, color=colors[ind], label=A.name)
+                axs[r].plot( A.left_endpt, A.function(A.left_endpt), color=colors[ind], marker="o" )
+                axs[r].plot( A.right_endpt, A.function(A.right_endpt), color=colors[ind], marker="o" )
+            axs[r].legend(bbox_to_anchor = [1, 0], loc = 'lower left' )
+            axs[r].set_title('Approval Functions')
+            self.setup_grid(axs[r], xmin, xmax, ymin, ymax, self.prec, tick)
+            r =+ 1
+
+        if plot_count_function:
+            self.plot_summary_function( axs[r], self.X, self.count_function, label='Count Function', tick=tick)
+            r += 1
+
+        if plot_binary_count:
+            self.plot_summary_function( axs[r], self.X, self.binary_count_function, label='Binary Count', tick=tick)
+            r += 1
+        if plot_sum_count:
+            self.plot_summary_function( 
+                axs[r], self.X, self.count_function+self.binary_count_function, 
+                    label='Sum of Count and Binary Count Functions', tick=tick
+            )
+
         if fig_path:
             plt.savefig(fig_path)
 
+    # Method to check (k, m) agreeability (brute force)
+    def check_agreeability( self, k, m):
+        set_names = [ A.name for A in self.approval_sets.values() ]
 
-    def plot_count_function( self, fig_path=None, xmin=0, xmax=10, ymin=0, ymax=5 ):
-        
-        ## Set up grid
-        fig = plt.figure()
-        ax = fig.add_subplot(1, 1, 1)
-        # Major ticks every 1, minor ticks every self.tick (default = 0.5)
-        major_ticks = np.arange(xmin, xmax, self.tick * 2)
-        minor_ticks = np.arange(xmin, xmax, self.tick)        
-        ax.set_xticks(major_ticks)
-        ax.set_xticks(minor_ticks, minor=True)
-        #ax.set_yticks(major_ticks)
-        #ax.set_yticks(minor_ticks, minor=True)
-        # Different thickness settings for the major and minor grids:
-        ax.grid(which='minor', alpha=0.2)
-        ax.grid(which='major', alpha=0.5)
-        plt.yticks(np.arange(0, self.num_voters+1, step = 1) )
+        # get all subsets of size m
+        subcollections_m = [list(i) for i in itertools.combinations(set_names, m)]
+        good_m_set_indicator = [] # is the society k,m agreeable? will store 1 or 0 for each collection of m sets
+        bad_m_sets = []
 
-        X = np.linspace(xmin, xmax, 1000)
-        Y = np.array([0 for pt in X])
+        # go through each subset of size m
+        for collection in subcollections_m:
 
-        # Draw the functions
-        for ind, A in enumerate( self.approval_sets ): 
-            Y = Y + [A.function(pt) for pt in X]
+            xmin = 0
+            xmax = 10
+            X = np.arange(self.xmin, self.xmax + self.prec, self.prec)
+            Y = np.zeros_like(X)
 
-        plt.plot( X, Y)
-        ax.set_ylim(ymin, ymax)
-        
-        if fig_path:
-            plt.savefig(fig_path)
+            # Compute count function
+            for set_name in collection:
+                A = self.get_approval_set(set_name)
+                Y = Y + np.array([A.function(pt) for pt in X])
+            if np.max(Y) >= k:
+                good_m_set_indicator.append(1)
+            else:
+                good_m_set_indicator.append(0)
+                bad_m_sets.append(collection)
 
+        good_m_set_indicator = np.array(good_m_set_indicator)
+        is_km_agreeable = np.prod(good_m_set_indicator) == 1
 
+        return is_km_agreeable, bad_m_sets
